803af3be1dbf8eb920bd6da46343d598a907d2de
dominik <docachaorg@gmail.com>
2025-11-09 00:45:14 +0100

    script

---
+++ run-demo.sh
@@ -0,0 +1,10 @@
+IMAGES=("frontend" "backend")
+TAG="latest"
+for APP in "${IMAGES[@]}"; do
+  echo "Building local image: ${APP}:${TAG}"
+  docker build -f "${APP}/Dockerfile" -t "${APP}:${TAG}" .
+done
+
+echo "Starting Demo"
+kubectl apply -f k8s.yaml
+kubectl -n demo-autoscale get pods -o wide
